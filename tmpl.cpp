4b72ada039a2bdec60f40364e10461370decc20b
mizunofukusayou <145898250+mizunofukusayou@users.noreply.github.com>
2026-09-19 19:49:04 +0900

    tmpl: add: 割り算結果を切り上げ

--- tmpl.cpp
+++ tmpl.cpp
@@ -42,6 +42,8 @@ void co(Args&&... args) {
 template<typename T> void print_vec(const vector<T> &v, bool split_line=false) {
     for (int i = 0; i < (int)v.size(); i++) cout << v[i] << " \n"[split_line || i+1==(int)v.size()];
 }
+
+template<typename T> T ceil(T a, T b) { return (a + (b - 1)) / b; }
 // clang-format on
 
 void solve() {}
